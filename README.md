07c07a1b8eff04873ce90dd907214c3f2cd70f7c
bilalcaliskan <bilalcaliskan@protonmail.com>
2021-08-02 23:38:51 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Golang Commons
+[![CI](https://github.com/vpnbeast/golang-commons/workflows/CI/badge.svg?event=push)](https://github.com/vpnbeast/golang-commons/actions?query=workflow%3ACI)
+[![Go Report Card](https://goreportcard.com/badge/github.com/vpnbeast/golang-commons)](https://goreportcard.com/report/github.com/vpnbeast/golang-commons)
+
+This is a common library for [Vpnbeast](https://github.com/vpnbeast) backend services which are written with Golang.
---
+++ pkg/options/options.go
@@ -0,0 +1,66 @@
+package options
+
+import (
+	"github.com/spf13/viper"
+	"github.com/vpnbeast/golang-commons/pkg/logging"
+	"go.uber.org/zap"
+	"os"
+	"reflect"
+	"strconv"
+)
+
+var logger *zap.Logger
+
+func init() {
+	logger = logging.GetLogger()
+}
+
+// UnmarshalConfig creates a new *viper.Viper and unmarshalls the config into struct using *viper.Viper
+func UnmarshalConfig(key string, opts interface{}) error {
+	sub := viper.Sub(key)
+	sub.AutomaticEnv()
+	sub.SetEnvPrefix(key)
+	// t := reflect.TypeOf(opts)
+	bindEnvs(sub, opts)
+	return sub.Unmarshal(opts)
+}
+
+// bindEnvs takes *viper.Viper as argument and binds structs fields to environments variables to be able to override
+// them using environment variables at the runtime
+func bindEnvs(sub *viper.Viper, opts interface{}) {
+	elem := reflect.ValueOf(opts).Type().Elem()
+	fieldCount := elem.NumField()
+	for i := 0; i < fieldCount; i++ {
+		env := elem.Field(i).Tag.Get("env")
+		name := elem.Field(i).Name
+		_ = sub.BindEnv(name, env)
+	}
+}
+
+// GetStringEnv gets the specific environment variables with default value, returns default value if variable not set
+func GetStringEnv(key, defaultValue string) string {
+	value := os.Getenv(key)
+	if len(value) == 0 {
+		return defaultValue
+	}
+	return value
+}
+
+// GetIntEnv gets the specific environment variables with default value, returns default value if variable not set
+func GetIntEnv(key string, defaultValue int) int {
+	value := os.Getenv(key)
+	if len(value) == 0 {
+		return defaultValue
+	}
+	return convertStringToInt(value)
+}
+
+func convertStringToInt(s string) int {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		logger.Warn("an error occurred while converting from string to int. Setting it as zero",
+			zap.String("error", err.Error()))
+		i = 0
+	}
+	return i
+}
